186ba5fb95269affd5d0e03d359a3f6d792f5cd0
Joe <jmk742@nyu.edu>
2016-10-04 14:34:51 -0400

    Session and User Controller complete

--- MediumCupOfJoe/app/controllers/api/session_controller.rb
+++ MediumCupOfJoe/app/controllers/api/session_controller.rb
@@ -1,10 +1,28 @@
 class Api::SessionController < ApplicationController
 
-  def method_name
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
 
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render(json: ["Invalid username/password"], status: 401)
+    end
   end
 
-  def current_user
-    @current_user = User.find_by(session_token: self.session_token)
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render(json: ["Nobody signed in"], status: 404)
+    end
   end
+
+
 end
--- MediumCupOfJoe/app/controllers/application_controller.rb
+++ MediumCupOfJoe/app/controllers/application_controller.rb
@@ -2,4 +2,29 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def signed_in?
+    !!current_user
+  end
+
+  def login!(user)
+    session[:token] = user.session_token
+    @current_user = user
+  end
+
+  def logout
+    current_user.try(:reset_session_token!)
+    session[:token] = nil
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session[:token])
+  end
+
+  def require_logged_in!
+    render json: {base: ['invalid credentials']}, status: 401 if !current_user
+  end
+
 end
