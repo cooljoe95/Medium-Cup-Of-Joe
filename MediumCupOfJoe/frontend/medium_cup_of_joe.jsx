9ff20de121ad1d47608a72660bd2c1badfb84737
Joe <jmk742@nyu.edu>
2016-10-04 12:13:05 -0400

    Landing Page works

---
+++ MediumCupOfJoe/frontend/medium_cup_of_joe.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Medium Cup Of Joe</h1>, root);
+});
