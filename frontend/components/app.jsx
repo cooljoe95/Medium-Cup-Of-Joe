f527fc0f28eeb0bfeec8f7364075a258220294eb
Joe Katz <jmk742@nyu.edu>
2016-10-05 09:08:01 -0400

    Root, Router, and App components added

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = ({ children }) => (
+  <div>
+    <h1>
+      Medium Cup Of Joe App
+    </h1>
+    {children}
+  </div>
+);
+
+export default App;
--- frontend/medium_cup_of_joe.jsx
+++ frontend/medium_cup_of_joe.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as SessionUtils from './actions/session_actions';
+import Root from './components/root';
+
 import configureStore from './store/store';
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    window.SAlogin = SessionUtils.login;
-    window.SAlogout = SessionUtils.logout;
-    window.SAsignup = SessionUtils.signup;
-    window.store = configureStore();
-    ReactDOM.render(<h1>Medium Cup Of Joe.</h1>, root);
+    const store = configureStore();
+    ReactDOM.render(<Root store={store}/>, root);
 });
