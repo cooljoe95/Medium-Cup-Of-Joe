4547edf5fb1af6b8bcd7fc9a5b860fbff9d11f00
Joe Katz <jmk742@nyu.edu>
2016-10-05 17:05:15 -0400

    Input and labels complete

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -20,7 +20,6 @@ class SessionForm extends React.Component {
   }
 
   componentDidUpdate(){
-    console.log(this.props.loggedIn);
     if(this.props.loggedIn) {
       hashHistory.push("/");
     }
@@ -42,21 +41,29 @@ class SessionForm extends React.Component {
 
     let link;
     let email;
+    let head;
     if(this.props.formType === "login"){
-      link = <Link to='/signup'>Sign up</Link>;
+      link = <Link to='/signup'>Sign up instead?</Link>;
+      head = <div>Enter your username and password to log in</div>;
     } else {
-       link = <Link to='/login'>Log in</Link>;
-      email = <input type="text" className="form-element" name="email" placeholder="email" value={this.state.email} onChange={this.updateState("email")}/>;
+      link = <Link to='/login'>Log in instead?</Link>;
+      head = <div>Enter your email address, username, and password to create an account on Medium Cup of Joe </div>;
+      email = [
+        <label className="input-label">Email address</label>,
+        <input type="text" className="form-element" name="email" placeholder="email" value={this.state.email} onChange={this.updateState("email")}/>
+      ];
     }
 
     return (
       <div className="jumbotron vertical-center">
         <div className="new-session">
           <div className="form-head">
-            <h2>Enter your email address to sign in or create an account on Medium Cup of Joe</h2>
+            {head}{link}
           </div>
           <form onSubmit={handleSubmit} className="new-session-form">
+            <label className="input-label">Username</label>
             <input type="text" value={this.state.username} name="username" className="form-element" placeholder="username" onChange={this.updateState("username")}/>
+            <label className="input-label">Password</label>
             <input type="password" name="password" value={this.state.password} placeholder="password" className="form-element" onChange={this.updateState("password")}/>
             {email}
             <input type="submit" className="form-element"/>
