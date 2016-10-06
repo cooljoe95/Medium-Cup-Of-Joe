4d0169c16f79b6e0e6bf86e3f7955aaf80d97586
Joe <jmk742@nyu.edu>
2016-10-06 15:17:46 -0400

    Get header to stay on top by itself, with logo on left and login/logout on the right

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,7 +3,7 @@ import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
   <div>
-    <div className="logo-container">
+    <div className="logo-container group">
       <p>Pic</p><GreetingContainer />
     </div>
     {children}
