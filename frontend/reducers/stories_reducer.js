d424fc9398e1f79faa739882015da34f15936b17
Joe <jmk742@nyu.edu>
2016-10-07 13:50:10 -0400

    Update stories reducer to give new state

--- frontend/reducers/stories_reducer.js
+++ frontend/reducers/stories_reducer.js
@@ -1,9 +1,14 @@
 import { RECEIVE_STORIES } from "../actions/story_actions";
+import merge from 'lodash/merge';
+
+const _defaultState = {
+  stories: {}
+};
 
 const StoriesReducer = (state = {}, action={}) => {
   switch (action.type) {
     case RECEIVE_STORIES:
-      return action.stories;
+      return merge(_defaultState, {stories: action.stories}).stories;
     default:
       return state;
   }
