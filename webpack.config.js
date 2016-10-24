5834cb6638e40649665e6fa58697ecbe7ff663dc
Joe Katz <jmk742@nyu.edu>
2016-10-24 10:58:28 -0400

    Minify possibly

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-          'NODE_ENV': prod? `"production"`: '"development"'
+          'NODE_ENV': '"' + process.env.NODE_ENV + '"'
       }
     })
   ]
