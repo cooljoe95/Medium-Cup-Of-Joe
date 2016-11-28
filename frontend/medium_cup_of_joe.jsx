896b9bf03ebccbe0bdb7104d135ee9e8681b546b
Joe Katz <jmk742@nyu.edu>
2016-11-27 23:14:41 -0500

    Got rid of stuff on window

--- frontend/medium_cup_of_joe.jsx
+++ frontend/medium_cup_of_joe.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-import { login } from './actions/session_actions';
-import { receiveStories, requestStories } from './actions/story_actions';
-import { fetchStories, fetchAuthors } from './util/story_api_util';
-
 import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
+  const root = document.getElementById('root');
     let store;
-    if(window.currentUser){
-      const preloadedState = {
-        currentUser: window.currentUser
-      };
+    if (window.currentUser) {
+      const preloadedState = { currentUser: window.currentUser };
       store = configureStore({session: preloadedState});
     } else {
       store = configureStore();
